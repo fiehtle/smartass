75b2791adfff4b633898b8194ff6acd04c1b3b95
viet <viet.dt.le@gmail.com>
2025-01-14 13:14:25 +0100

    Initial Commit

---
+++ smartass/smartassApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartassApp.swift
+//  smartass
+//
+//  Created by Viet Le on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct smartassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
